4a5be46192a928efade9b00bfd62ab34753f840f
nugent.jack <nugent.jack@M6NN03W2>
2021-05-24 16:31:54 -0400

    Copy Server example from HotChocolate

---
+++ Query.cs
@@ -0,0 +1,17 @@
+﻿namespace HotChocolate.Server.Template
+{
+    public class Query
+    {
+        public Person GetPerson() => new Person("Luke Skywalker");
+    }
+
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
